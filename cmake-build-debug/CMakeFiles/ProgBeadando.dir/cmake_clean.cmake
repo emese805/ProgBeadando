d507951395df1625f9170d910acb490e7cecf90d
sanyikarcsi <sanyikarcsi@gmail.com>
2018-02-27 19:22:23 +0100

    ASSD

---
+++ cmake-build-debug/CMakeFiles/ProgBeadando.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProgBeadando.dir/main.cpp.obj"
+  "ProgBeadando.pdb"
+  "ProgBeadando.exe"
+  "ProgBeadando.exe.manifest"
+  "libProgBeadando.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProgBeadando.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
